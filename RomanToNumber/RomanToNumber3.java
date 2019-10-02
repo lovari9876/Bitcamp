6cf6e9f36083a3c1e3737341a43f2f64ab515437
Kong Miseon <lovari9876@gmail.com>
2019-10-02 15:19:30 +0900

    update git

---
+++ RomanToNumber/RomanToNumber3.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+class RomanToNumber2 {
+
+	public static void main(String[] args) throws Exception {
+	    Scanner scanner = new Scanner(System.in);
+	    //스캐너는 현재 heap에 생긴 상태
+
+	    System.out.print("input roman number: ");
+	    String roman = scanner.next();
+
+	    //대소문자구분을 못하므로 예외사항에 대한 대응으로 대소문자 바꾸는 코드를 넣어준다.
+	    if(roman.equalsIgnoreCase("I")) {
+		System.out.println("number 1");
+	    } else if (roman.equalsIgnoreCase("II")) {
+		System.out.println("number 2");
+	    } else if (roman.equalsIgnoreCase("III")) {
+		System.out.println("number 3");
+	    } else if (roman.equalsIgnoreCase("IV")) {
+		System.out.println("number 4");
+	    } else if (roman.equalsIgnoreCase("V")) {
+		System.out.println("number 5");
+	    } else if (roman.equalsIgnoreCase("VI")) {
+		System.out.println("number 6");
+	    } else if (roman.equalsIgnoreCase("VII")) {
+		System.out.println("number 7");
+	    } else if (roman.equalsIgnoreCase("VIII")) {
+		System.out.println("number 8");
+	    } else if (roman.equalsIgnoreCase("IX")) {
+		System.out.println("number 9");
+	    } else if (roman.equalsIgnoreCase("X")) {
+		System.out.println("number 10");
+	    } else {
+		System.out.println("Invalid roman") 
+	    }	    
+	    	    
+	    /*
+	    System.out.println("Roman number: " + roman);
+	    System.out.println("toLowerCase: " + roman.toLowerCase());
+	    System.out.println("toUpperCase: " + roman.toUpperCase());
+	    System.out.println("\"I\" == \"I\": " + ("I"=="I"));
+	    System.out.println("compareTo(\"I\"): " + roman.compareTo("I"));
+	    System.out.println("equals(\"I\"): " + roman.equals("i"));
+	    System.out.println("equalsIgnoreCase(\"I\"): " + equalsIgnoreCase("i"));
+	    */
+
+	    
+	    /*
+	    if (roman == "I") {
+		System.out.println("number 1");
+	    } else if (roman == "II") {
+		System.out.println("number 2");
+	    } else if (roman == "III") {
+		System.out.println("number 3");
+	    } else if (roman == "IV") {
+		System.out.println("number 4");
+	    } else if (roman == "V") {
+		System.out.println("number 5");
+	    } else if (roman == "VI") {
+		System.out.println("number 6");
+	    } else if (roman == "VII") {
+		System.out.println("number 7");
+	    } else if (roman == "VIII") {
+		System.out.println("number 8");
+	    } else if (roman == "IX") {
+		System.out.println("number 9");
+	    } else if (roman == "X") {
+		System.out.println("number 10");
+	    }
+    	    */
+
+
+	}
+}
